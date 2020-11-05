62c65fb4f767e65211ddc63fd2d3a74af22f592a
Student   MALONE James <06541@farnborough.ac.uk>
2020-11-05 13:40:34 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main{
+    public static void main(String[] args) {
+        String test = "Test1";
+        System.out.println(test);
+        System.out.println(test.length());
+        System.out.println("test");
+
+    }
+}
